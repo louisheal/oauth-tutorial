346a5e86b9046c7ed49b2cc654c6464d8da766c0
Louis Heal <louisheal@live.co.uk>
2024-11-23 20:20:28 +0000

    create routes template

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Response
+from fastapi.responses import RedirectResponse
+import secrets
 
 from app.models import OAuthProvider
 
@@ -7,15 +9,21 @@ class Routes():
   
   def __init__(self, oauth_provider: OAuthProvider) -> None:
     self._router = APIRouter()
-    self.oauth = oauth_provider
+    self._oauth = oauth_provider
 
-    @self._router.get('/login')
+    @self._router.get("/login")
     async def get_login():
-      pass
 
-    @self._router.get('/callback')
-    async def get_callback():
-      pass
+      # Write your code here #
+      ...
+      ########################
+
+    @self._router.get("/callback")
+    async def get_callback(request: Request, code: str, state: str):
+      
+      # Write your code here #
+      ...
+      ########################
     
   def router(self) -> APIRouter:
     return self._router
